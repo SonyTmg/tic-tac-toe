ae1a32af5a912d30f389a6db07cd68f90b2b8a79
Sony Tamang <sony123ktm@gmail.com>
2023-03-01 14:46:02 +1100

    replace style with class

--- src/Grid.js
+++ src/Grid.js
@@ -3,28 +3,28 @@ import './App.css';
 function Grid() {
   return (
     <div>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
+      <div className="grid-row" >
+        <div className="grid-tile" >X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
       </div>
-      <div style={{borderTop: '1px solid white'}}></div>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
+      <div className="grid-border-top"></div>
+      <div className="grid-row" >
+        <div className="grid-tile">X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
       </div>
-      <div style={{borderTop: '1px solid white'}}></div>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
-        <div style={{borderRight: '1px solid white'}}></div>
-        <div style={{padding: 20}}>X</div>
+      <div className="grid-border-top"></div>
+      <div className="grid-row" >
+        <div className="grid-tile">X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
+        <div className="grid-border-right" ></div>
+        <div className="grid-tile">X</div>
       </div>
     </div>
   );
